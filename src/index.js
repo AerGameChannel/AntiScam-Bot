ab133b55a6fa34f536153a1eae51ae6cc53af7c9
Justin SpielefreakJ <github@spielefreakj.com>
2022-01-24 22:01:49 +0100

    added try-catch + new replace to have the link not clickable as well as shown in audit logs and embed
    added try catch if delete or ban perms are not given to the bot
    
    additionally added {MESSAGE} replacement to show the message content in the embed and the discord autit log (and not having the link clickable in the link)

--- src/index.js
+++ src/index.js
@@ -33,7 +33,9 @@ client.on('messageCreate', async (message) => {
   const logChannel = config.logChannel || message.channel;
   const logEmbedDesc = config.embedOptions.description
     .replace(/{MENTION}/g, message.author.tag)
-    .replace(/{ID}/g, message.author.id);
+    .replace(/{ID}/g, message.author.id)
+    .replace(/{MESSAGE}/g, message.content)
+    .replace ("://", ": //");
   const logEmbed = new MessageEmbed()
     .setColor(config.embedOptions.color)
     .setAuthor(message.author.tag, message.author.displayAvatarURL())
@@ -46,12 +48,33 @@ client.on('messageCreate', async (message) => {
 
   switch (config.action) {
     case 'DELETE':
-      await message.delete({ reason: config.actionReason });
-      break;
+      try{
+        await message.delete({ reason: config.actionReason });
+        console.log(`Blocked scam link. ${log}`);
+      }
+      catch (error){
+        console.error(error);
+        await logChannel.send(`Error occured. Message not deleted or not found`);
+      }
 
     case 'BAN':
-      await message.delete({ reason: config.actionReason });
-      await message.guild.bans.create(message.author, { reason: config.actionReason });
+      try{
+        await message.delete({ reason: config.actionReason });
+        console.log(`Blocked scam link. ${log}`);
+      }
+      catch (error){
+        console.error(error);
+        await logChannel.send(`Error occured. Message not deleted or not found`);
+      }
+      try{
+        const logBAN = config.actionReason
+          .replace(/{MESSAGE}/g, message.content);
+        await message.guild.bans.create(message.author, { reason: logBAN});
+      }
+      catch (error){
+        console.error(error);
+        await logChannel.send(`Error occured. User likely not banned! (Perm error?)`);
+      }
       break;
 
     case 'IGNORE':
